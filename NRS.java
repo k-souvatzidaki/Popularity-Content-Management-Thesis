87434292c93a3dfcafa6311b59eaa9cc93c06914
k-souvatzidaki <kwn.souv@gmail.com>
2021-04-02 18:19:35 +0300

    count false positives

--- NRS.java
+++ NRS.java
@@ -5,12 +5,12 @@ import java.math.RoundingMode;
 /* A Name Resolution System (NRS) */
 public class NRS {
 
-    final int total_naps = 10; //the total number of NAPs
-    final int bloom_size = 10; //the size of the bloom filters
+    final int total_naps = 20; //the total number of NAPs
+    final int bloom_size = 30; //the size of the bloom filters
     final int bloom_hashes = 1; //how many times ids are hashed in the bloom filter
-    final int total_ids = 100; //the total number of content ids to be generated
+    final int total_ids = 1000; //the total number of content ids to be generated
     final double exponent =  1.3; //the Zipf distribution exponent
-    final int total_queries = 1000000; //the number of queries to be generated
+    final int total_queries = 100; //the number of queries to be generated
 
     int false_positives = 0; //total number of false positives from content queries to NAPs
     NAP[] naps;
@@ -52,7 +52,7 @@ public class NRS {
     }
 
     public void start() {
-        System.out.println("START GENERATING QUERIES . . .");
+        System.out.println("\nSTART GENERATING QUERIES . . .");
         BigDecimal temp; int rank; String id;
         //Zipfian distribution for popularity aware query generation
         Zipf zipf = new Zipf(exponent,total_ids);
@@ -62,24 +62,38 @@ public class NRS {
             System.out.println("for rank = "+a+" zipf = "+zipf.getZipfVal(a));
         }
         */
-
+        int false_pos;
         for(int k = 0; k < total_queries; k++) {
             //get a content id based on popularity rank
             temp = new BigDecimal("0.0");
             while(temp.compareTo(zipf.getZipfVal(total_ids)) < 0)
                 temp = new BigDecimal(Math.random()).setScale(32,RoundingMode.HALF_EVEN); 
             rank = zipf.getRank(temp);
-            if(rank > 100) {
-                System.out.println(rank);
-                System.out.println(temp);
-            }
             id = ids.get(rank-1); counters[rank-1]++;
+
+            //query execution - check bloom filters for the id
+            false_pos = 0;
+            for(NAP nap: naps) {
+                //check if id exists in bloom filter
+                if(nap.update().exists(id)) {
+                    //check if id actually exists in nap
+                    if(nap.isAttached(id)) {
+                        System.out.println("Found id "+id+" in NAP "+nap.id()+" after "+false_pos+" tries");
+                        break;
+                    } else false_pos++;
+                }
+            }
+            //update total false positives
+            false_positives += false_pos;
+
             //System.out.println("Query #"+k+" for content id \'"+id+"\' with rank "+rank+"/"+total_ids);
         }
         
         System.out.println("QUERY GENERATION COMPLETED. RESULTS: ");
         System.out.println("QUERIES PER RANK");
         for(int c = 0; c < total_ids; c++) System.out.println("Rank #"+(c+1)+" : "+counters[c]+" queries");
+
+        System.out.println("\nTotal false positives: "+ false_positives);
     }
 
     public static void main(String[] args) {
